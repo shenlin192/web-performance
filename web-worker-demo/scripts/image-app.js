372faf3bafafd71358bebc0d0b4d779cdb069a40
lsn <lsn@one2team.com>
2019-05-03 00:30:24 +0200

    update web worker demo

--- web-worker-demo/scripts/image-app.js
+++ web-worker-demo/scripts/image-app.js
@@ -1,6 +1,6 @@
 (function(){
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
-  var worker = new Worker('http://localhost:5000/scripts/worker.js');
+  // var worker = new Worker('http://localhost:5000/scripts/worker.js');
   var original;
   var imageLoader = document.querySelector('#imageLoader');
   imageLoader.addEventListener('change', handleImage, false);
@@ -41,27 +41,28 @@
 
     toggleButtonsAbledness();
 
-    worker.postMessage({'imageData': imageData, 'type': type});
+    // worker.postMessage({'imageData': imageData, 'type': type});
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    // length = imageData.data.length / 4;
-    // for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-    //   r = imageData.data[i * 4 + 0];
-    //   g = imageData.data[i * 4 + 1];
-    //   b = imageData.data[i * 4 + 2];
-    //   a = imageData.data[i * 4 + 3];
-    //   pixel = manipulate(type, r, g, b, a);
-    //   imageData.data[i * 4 + 0] = pixel[0];
-    //   imageData.data[i * 4 + 1] = pixel[1];
-    //   imageData.data[i * 4 + 2] = pixel[2];
-    //   imageData.data[i * 4 + 3] = pixel[3];
-    // }
+    length = imageData.data.length / 4;
+    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+      r = imageData.data[i * 4 + 0];
+      g = imageData.data[i * 4 + 1];
+      b = imageData.data[i * 4 + 2];
+      a = imageData.data[i * 4 + 3];
+      pixel = manipulate(type, r, g, b, a);
+      imageData.data[i * 4 + 0] = pixel[0];
+      imageData.data[i * 4 + 1] = pixel[1];
+      imageData.data[i * 4 + 2] = pixel[2];
+      imageData.data[i * 4 + 3] = pixel[3];
+    }
     toggleButtonsAbledness();
-    worker.addEventListener('message', function(e) {
-      ctx.putImageData(e.data, 0, 0);
-    }, false);
+    return ctx.putImageData(imageData, 0, 0);
+    // worker.addEventListener('message', function(e) {
+    //   ctx.putImageData(e.data, 0, 0);
+    // }, false);
   };
 
   function revertImage() {
